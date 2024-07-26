4932e45e4fab525e031968d5af0586d82db32204
Non T. <117605943+Ratanon691@users.noreply.github.com>
2024-07-26 10:35:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+function doubleAndReturnArgs(array, ...numbers) {
+  const doubledNumbers = numbers.map(num => num * 2)
+  return [...array, ...doubledNumbers]
+}
+
+console.log(doubleAndReturnArgs([1, 2, 3], 4, 4)); 
+// expected result: [1, 2, 3, 8, 8]
+
+console.log(doubleAndReturnArgs([2], 10, 4)); 
+// expected result: [2, 20, 8]
